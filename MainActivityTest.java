c14a025a44acae378faf8bfd639a65701b0f65ac
leroy <leroy.c.dunn@gmail.com>
2017-07-26 00:54:35 +0200

    init

---
+++ MainActivityTest.java
@@ -0,0 +1,98 @@
+package com.example.app;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+	@Rule
+	public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+	@Test public void t8(){}
+	@Test public void t9(){}
+	@Test public void t10(){}
+	@Test public void t11(){}
+	@Test public void t12(){}
+	@Test public void t13(){}
+	@Test public void t14(){}
+	@Test public void t15(){}
+	@Test public void t16(){}
+	@Test public void t17(){}
+	@Test public void t18(){}
+	@Test public void t19(){}
+	@Test public void t20(){}
+	@Test public void t21(){}
+	@Test public void t22(){}
+	@Test public void t23(){}
+	@Test public void t24(){}
+	@Test public void t25(){}
+	@Test public void t26(){}
+	@Test public void t27(){}
+	@Test public void t28(){}
+	@Test public void t29(){}
+	@Test public void t30(){}
+	@Test public void t31(){}
+	@Test public void t32(){}
+	@Test public void t33(){}
+	@Test public void t34(){}
+	@Test public void t35(){}
+	@Test public void t36(){}
+	@Test public void t37(){}
+	@Test public void t38(){}
+	@Test public void t39(){}
+	@Test public void t40(){}
+	@Test public void t41(){}
+	@Test public void t42(){}
+	@Test public void t43(){}
+	@Test public void t44(){}
+	@Test public void t45(){}
+	@Test public void t46(){}
+	@Test public void t47(){}
+	@Test public void t48(){}
+	@Test public void t49(){}
+	@Test public void t218(){}
+	@Test public void t219(){}
+	@Test public void t110(){}
+	@Test public void t111(){}
+	@Test public void t112(){}
+	@Test public void t113(){}
+	@Test public void t114(){}
+	@Test public void t115(){}
+	@Test public void t116(){}
+	@Test public void t117(){}
+	@Test public void t118(){}
+	@Test public void t119(){}
+	@Test public void t120(){}
+	@Test public void t121(){}
+	@Test public void t122(){}
+	@Test public void t123(){}
+	@Test public void t124(){}
+	@Test public void t125(){}
+	@Test public void t126(){}
+	@Test public void t127(){}
+	@Test public void t128(){}
+	@Test public void t129(){}
+	@Test public void t130(){}
+	@Test public void t131(){}
+	@Test public void t132(){}
+	@Test public void t133(){}
+	@Test public void t134(){}
+	@Test public void t135(){}
+	@Test public void t136(){}
+	@Test public void t137(){}
+	@Test public void t138(){}
+	@Test public void t139(){}
+	@Test public void t140(){}
+	@Test public void t141(){}
+	@Test public void t142(){}
+	@Test public void t143(){}
+	@Test public void t144(){}
+	@Test public void t145(){}
+	@Test public void t146(){}
+	@Test public void t147(){}
+	@Test public void t148(){}
+	@Test public void t149(){}
+	
+}
